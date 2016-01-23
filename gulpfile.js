ec8c027f7236d14c1e6fb08807b5295c0068c093
Robert Down <robertdown@live.com>
2016-01-22 21:38:51 -0500

    Moving to Gulp

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+var gulp = require('gulp'),     
+    sass = require('gulp-ruby-sass') 
+    notify = require("gulp-notify") 
+    bower = require('gulp-bower');
+
+var config = {
+     sassPath: './sass',
+     bowerDir: './bower_components' 
+};
+
+gulp.task('bower', function() { 
+    return bower()
+         .pipe(gulp.dest(config.bowerDir)) 
+});
+
+gulp.task('css', function() { 
+    return gulp.src(config.sassPath + '/style.scss')
+         .pipe(sass({
+             style: 'compressed',
+             loadPath: [
+                 './sass',
+                 config.bowerDir + '/bootstrap-sass/assets/stylesheets',
+             ]
+         }) 
+            .on("error", notify.onError(function (error) {
+                 return "Error: " + error.message;
+             }))) 
+         .pipe(gulp.dest('./public/css')); 
+});
+
+// Rerun the task when a file changes
+ gulp.task('watch', function() {
+     gulp.watch(config.sassPath + '/**/*.scss', ['css']); 
+});
+
+  gulp.task('default', ['bower', 'css']);
